7d3dd179e50f7d0060bfa9fca7b6c24331f0742a
Leo <www.krasavinlv@gmail.com>
2026-09-15 21:06:37 +0300

    fix:added email to migrations

--- backend/migrations/001_create_users.up.sql
+++ backend/migrations/001_create_users.up.sql
@@ -2,6 +2,8 @@ CREATE TABLE users (
     id BIGSERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     password_hash VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE
+        CHECK (email ~* '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$'),
     role VARCHAR(20) NOT NULL DEFAULT 'user',
 
     CONSTRAINT users_role_check
