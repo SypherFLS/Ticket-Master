e7c681b52107d1dc338484579219bb181bf5d94e
Leo <www.krasavinlv@gmail.com>
2026-09-17 07:09:56 +0300

    feat:auth added

--- backend/internal/auth/jwt.go
+++ backend/internal/auth/jwt.go
@@ -1,11 +1,66 @@
 package auth
 
+import (
+	"time"
+	"tmaster/internal/apperrors"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
 type JWTManager struct {
-	Secret string
+	secret []byte
 }
 
-func NewJWTManager(secret string) *JWTManager {
+func NewJWTManager(secret []byte) *JWTManager {
 	return &JWTManager {
-		Secret: secret,
+		secret: secret,
 	}
 }
+
+type claims struct {
+	UserID int `json:"user_id"`
+
+	jwt.RegisteredClaims
+}
+
+func (j *JWTManager) Generate(userID int) (string, error) {
+	claims := claims{
+		UserID : userID,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(
+				time.Now().Add(24 * time.Hour),
+			),
+		},
+	}
+	token := jwt.NewWithClaims(
+		jwt.SigningMethodHS256,
+		claims,
+	)
+
+	return token.SignedString(j.secret)
+}
+
+func (j *JWTManager) Validate(tokenString string) (int, error) {
+	claims := &claims{}
+
+	token, err := jwt.ParseWithClaims(tokenString, claims,
+		func(token *jwt.Token) (any, error) {
+			if token.Method != jwt.SigningMethodHS256 {
+				return nil, apperrors.WrongSignMethod
+			}
+
+			return j.secret, nil
+		},
+	)
+
+	if err != nil {
+		return 0, err
+	}
+
+	if !token.Valid {
+		return 0, apperrors.InvalidToken
+	}
+
+	return claims.UserID, nil
+}
+
