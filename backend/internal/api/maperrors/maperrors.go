9a7779d9bf1d067799698dd8170dc9f8e2326772
Leo <www.krasavinlv@gmail.com>
2026-09-23 22:14:21 +0300

    feat:error mapping added

--- backend/internal/api/maperrors/maperrors.go
+++ backend/internal/api/maperrors/maperrors.go
@@ -10,6 +10,12 @@ func StatusFromErr(err error) int {
 	switch {
 	case errors.Is(err, apperrors.WrongSignMethod) || errors.Is(err, apperrors.InvalidToken) || errors.Is(err, apperrors.BlankUserID) || errors.Is(err, apperrors.WrongParamType):
 		return http.StatusUnauthorized
+	case errors.Is(err, apperrors.TicketQueueIsEmpty):
+		return http.StatusOK // because it is not a real service error
+	case errors.Is(err, apperrors.NoPermission):
+		return http.StatusForbidden
+	case errors.Is(err, apperrors.NothingChanged):
+		return http.StatusBadRequest
 	default:
 		return http.StatusInternalServerError
 	}
--- backend/internal/apperrors/apperrors.go
+++ backend/internal/apperrors/apperrors.go
@@ -3,8 +3,11 @@ package apperrors
 import "errors"
 
 var (
-	WrongSignMethod = errors.New("wrong sign method")
-	InvalidToken    = errors.New("invalid token")
-	BlankUserID     = errors.New("blank user id")
-	WrongUserID     = errors.New("wrong user id")
+	WrongSignMethod    = errors.New("wrong sign method")
+	InvalidToken       = errors.New("invalid token")
+	BlankUserID        = errors.New("blank user id")
+	WrongParamType     = errors.New("wrong param type")
+	TicketQueueIsEmpty = errors.New("ticket queue is empty")
+	NoPermission       = errors.New("no permisssion")
+	NothingChanged     = errors.New("nothing changed")
 )
